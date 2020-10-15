c7e370644b56d30242afd1408a5e1846b0b6d2d5
Tadashi-Croy <Tadashicroy@gmail.com>
2020-10-14 18:03:42 -0700

    added drop down

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
+// let dropDownComp = Vue.component('drop-down', {
+    
+//     props:['words', 'link'],
+//     data: function(){
+
+//         return {shown: true}
+
+//     },
+//     methods:{
+//         dropper: function(){
+//            this.shown = !this.shown
+//         }
+//     },
+//     template:`
+//       <div v-on:click = '$emit("drop()")'> <q>{{words}} {{this.shown}}</q><p>-<a v-bind:href='link'> </a></p> </div>
+//     `
+// })
+
+
+
+
+
+
+
+
+
 let app = new Vue({
     el: '#app',
     data: {
         message: "",
         seer: false,
+        drop1: true,
+        drop2: true
 
     },
     created(){
